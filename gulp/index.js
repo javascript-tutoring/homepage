c8ee8c32df24b80e1e3e59b8d83e41a9903302dd
Vitaliy Isikov <visikov@gmail.com>
2020-09-10 13:23:45 -0400

    Fix server and js compile

--- gulp/index.js
+++ gulp/index.js
@@ -1,16 +1,50 @@
+const path = require('path')
+
 const gulp = require('gulp')
+const clean = require('gulp-clean')
+const concat = require('gulp-concat')
+const sourcemaps = require('gulp-sourcemaps')
+const rollup = require('gulp-better-rollup')
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const commonjs = require('@rollup/plugin-commonjs')
+const babel = require('rollup-plugin-babel')
 const sass = require('gulp-sass')
 sass.compiler = require('node-sass')
 
+const ROOT = path.resolve(process.cwd())
+
+const globs = {
+  sass: path.join(ROOT, './src/sass/**/*.scss'),
+  js: path.join(ROOT, './src/js/**/*.js'),
+  html: path.join(ROOT, './src/index.html'),
+}
+
+gulp.task('clean', () => gulp.src(path.join(ROOT, './dist/*')).pipe(clean()))
+
 gulp.task('sass', () =>
   gulp
-    .src('../src/sass/**/*.scss')
+    .src(globs.sass)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('../dist/css')),
+    .pipe(gulp.dest(path.join(ROOT, './dist/css'))),
 )
 
-gulp.task('html', () =>
-  gulp.src('../src/index.html').pipe(gulp.dest('../dist')),
+gulp.task('js', () =>
+  gulp
+    .src(globs.js)
+    .pipe(sourcemaps.init())
+    .pipe(rollup({ plugins: [nodeResolve(), commonjs(), babel({ presets: ['@babel/env'] })] }, { format: 'cjs' }))
+    .pipe(sourcemaps.write())
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest(path.join(ROOT, './dist/js'))),
 )
 
-exports.build = gulp.task('build', () => gulp.parallel('html', 'sass')
+gulp.task('html', () => gulp.src(globs.html).pipe(gulp.dest(path.join(ROOT, './dist'))))
+
+gulp.task('build', gulp.series(gulp.parallel('sass', 'js'), 'html'))
+
+gulp.task(
+  'watch',
+  gulp.series('clean', 'build', () => gulp.watch(Object.values(globs), gulp.series('clean', 'build'))),
+)
+
+exports.default = gulp.series('clean', 'build')
---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import pretty from 'console.pretty'
+
+pretty.red('**********************************************************')
+pretty.red('**********************************************************')
+pretty.green('Interested in javascript? css? html? Join us in our slack!')
+pretty.blue('https://join.slack.com/t/javascripttutoring/shared_invite/zt-gihxhp1h-CONRSdy~KbB_x5FPM3L7NA')
+pretty.red('**********************************************************')
+pretty.red('**********************************************************')
